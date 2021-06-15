0256b6818dd023acc0e211e71d103ac7fbe02739
Yash Totale <totaleyash@gmail.com>
2021-06-15 12:08:31 -0700

    Add '--output_dir' argument

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -6,73 +6,99 @@ import bs4
 
 from scraper import scrape_book
 
-RATING_STARS_DICT = {'it was amazing': 5,
-                     'really liked it': 4,
-                     'liked it': 3,
-                     'it was ok': 2,
-                     'did not like it': 1,
-                     '': None}
+RATING_STARS_DICT = {
+    "it was amazing": 5,
+    "really liked it": 4,
+    "liked it": 3,
+    "it was ok": 2,
+    "did not like it": 1,
+    "": None,
+}
 
 
 def create_url(user_id, shelf, page):
-    return 'https://www.goodreads.com/review/list/' + user_id + '?shelf=' + shelf + '&page=' + str(page) + '&print=true'
+    return (
+        "https://www.goodreads.com/review/list/"
+        + user_id
+        + "?shelf="
+        + shelf
+        + "&page="
+        + str(page)
+        + "&print=true"
+    )
 
 
 def get_id(book_row):
-    cell = book_row.find('td', {'class': 'field title'})
-    title_href = cell.find('div', {'class': 'value'}).find('a')
-    return title_href.attrs.get('href').split('/')[-1]
+    cell = book_row.find("td", {"class": "field title"})
+    title_href = cell.find("div", {"class": "value"}).find("a")
+    return title_href.attrs.get("href").split("/")[-1]
 
 
 def get_rating(book_row):
-    cell = book_row.find('td', {'class': 'field rating'})
-    str_rating = cell.find('div', {'class': 'value'}).find('span').attrs.get('title')
+    cell = book_row.find("td", {"class": "field rating"})
+    str_rating = cell.find("div", {"class": "value"}).find("span").attrs.get("title")
     return RATING_STARS_DICT.get(str_rating)
 
 
 def get_dates_read(book_row):
-    cell = book_row.find('td', {'class': 'field date_read'})
-    dates = cell.find('div', {'class': 'value'}).findChildren('div', {'class': 'date_row'})
+    cell = book_row.find("td", {"class": "field date_read"})
+    dates = cell.find("div", {"class": "value"}).findChildren(
+        "div", {"class": "date_row"}
+    )
     date_arr = []
     for date in dates:
         date_text = date.text.strip()
-        if date_text != 'not set':
+        if date_text != "not set":
             date_arr += [date_text]
     return date_arr
 
 
-def scrape_user(user_id):
+def scrape_user(args):
+    user_id = args.user_id
+    output_dir = args.output_dir
     page = 1
-    while True:
-        url = create_url(user_id, 'read', page)
-        source = urlopen(url)
-        soup = bs4.BeautifulSoup(source, 'html.parser')
 
-        no_content = soup.find('div', {'class': 'greyText nocontent stacked'})
+    while True:
+        url = create_url(user_id, "read", page)
+        source = urlopen(url)
+        soup = bs4.BeautifulSoup(source, "html.parser")
+
+        no_content = soup.find("div", {"class": "greyText nocontent stacked"})
         if no_content:
             break
 
-        books_table = soup.find('tbody', {'id': 'booksBody'})
-        book_rows = books_table.findChildren('tr', recursive=False)
+        books_table = soup.find("tbody", {"id": "booksBody"})
+        book_rows = books_table.findChildren("tr", recursive=False)
 
         for book_row in book_rows:
             book_id = get_id(book_row)
             book = scrape_book.scrape_book(book_id)
-            book['rating'] = get_rating(book_row)
-            book['dates_read'] = get_dates_read(book_row)
-            json.dump(book, open('books/read/' + book.get('book_id_title') + '.json', 'w'), indent=2)
-            print("🎉 Scraped " + book.get('book_id_title'))
+            book["rating"] = get_rating(book_row)
+            book["dates_read"] = get_dates_read(book_row)
+            json.dump(
+                book,
+                open(
+                    output_dir
+                    + "/read/"
+                    + book.get("book_id_title")
+                    + ".json",
+                    "w",
+                ),
+                indent=2,
+            )
+            print("🎉 Scraped " + book.get("book_id_title"))
 
         page += 1
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--user_id', type=str)
+    parser.add_argument("--user_id", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, default="books")
     args = parser.parse_args()
 
-    scrape_user(args.user_id)
+    scrape_user(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- scraper/scrape_book.py
+++ scraper/scrape_book.py
@@ -9,9 +9,11 @@ import bs4
 
 def get_genres(soup):
     genres = []
-    for node in soup.find_all('div', {'class': 'left'}):
-        current_genres = node.find_all('a', {'class': 'actionLinkLite bookPageGenreLink'})
-        current_genre = ' > '.join([g.text for g in current_genres])
+    for node in soup.find_all("div", {"class": "left"}):
+        current_genres = node.find_all(
+            "a", {"class": "actionLinkLite bookPageGenreLink"}
+        )
+        current_genre = " > ".join([g.text for g in current_genres])
         if current_genre.strip():
             genres.append(current_genre)
     return genres
@@ -20,7 +22,7 @@ def get_genres(soup):
 def get_series_name(soup):
     series = soup.find(id="bookSeries").find("a")
     if series:
-        series_name = re.search(r'\((.*?)\)', series.text).group(1)
+        series_name = re.search(r"\((.*?)\)", series.text).group(1)
         return series_name
     else:
         return None
@@ -36,35 +38,37 @@ def get_series_uri(soup):
 
 
 def get_rating_distribution(soup):
-    distribution = re.findall(r'renderRatingGraph\([\s]*\[[0-9,\s]+', str(soup))[0]
-    distribution = ' '.join(distribution.split())
-    distribution = [int(c.strip()) for c in distribution.split('[')[1].split(',')]
-    distribution_dict = {5: distribution[0],
-                         4: distribution[1],
-                         3: distribution[2],
-                         2: distribution[3],
-                         1: distribution[4]}
+    distribution = re.findall(r"renderRatingGraph\([\s]*\[[0-9,\s]+", str(soup))[0]
+    distribution = " ".join(distribution.split())
+    distribution = [int(c.strip()) for c in distribution.split("[")[1].split(",")]
+    distribution_dict = {
+        5: distribution[0],
+        4: distribution[1],
+        3: distribution[2],
+        2: distribution[3],
+        1: distribution[4],
+    }
     return distribution_dict
 
 
 def get_num_pages(soup):
-    if soup.find('span', {'itemprop': 'numberOfPages'}):
-        num_pages = soup.find('span', {'itemprop': 'numberOfPages'}).text.strip()
+    if soup.find("span", {"itemprop": "numberOfPages"}):
+        num_pages = soup.find("span", {"itemprop": "numberOfPages"}).text.strip()
         return int(num_pages.split()[0])
-    return ''
+    return ""
 
 
 def get_year_first_published(soup):
-    year_first_published = soup.find('nobr', attrs={'class': 'greyText'})
+    year_first_published = soup.find("nobr", attrs={"class": "greyText"})
     if year_first_published:
         year_first_published = year_first_published.string
-        return re.search('([0-9]{3,4})', year_first_published).group(1)
+        return re.search("([0-9]{3,4})", year_first_published).group(1)
     else:
         return None
 
 
 def get_description(soup):
-    return soup.find('div', {'id': 'description'}).findAll('span')[-1].text
+    return soup.find("div", {"id": "description"}).findAll("span")[-1].text
 
 
 def get_id(book_id):
@@ -73,24 +77,32 @@ def get_id(book_id):
 
 
 def scrape_book(book_id):
-    url = 'https://www.goodreads.com/book/show/' + book_id
+    url = "https://www.goodreads.com/book/show/" + book_id
     source = urlopen(url)
-    soup = bs4.BeautifulSoup(source, 'html.parser')
+    soup = bs4.BeautifulSoup(source, "html.parser")
 
-    return {'book_id_title': book_id,
-            'book_id': get_id(book_id),
-            'book_title': ' '.join(soup.find('h1', {'id': 'bookTitle'}).text.split()),
-            'book_description': get_description(soup),
-            'book_url': url,
-            'book_image': soup.find('img', {'id': 'coverImage'}).attrs.get('src'),
-            'book_series': get_series_name(soup),
-            'book_series_uri': get_series_uri(soup),
-            'year_first_published': get_year_first_published(soup),
-            'author': ' '.join(soup.find('span', {'itemprop': 'name'}).text.split()),
-            'author_url': soup.find('a', {'class': 'authorName'}).attrs.get('href'),
-            'num_pages': get_num_pages(soup),
-            'genres': get_genres(soup),
-            'num_ratings': int(soup.find('meta', {'itemprop': 'ratingCount'})['content'].strip()),
-            'num_reviews': int(soup.find('meta', {'itemprop': 'reviewCount'})['content'].strip()),
-            'average_rating': float(soup.find('span', {'itemprop': 'ratingValue'}).text.strip()),
-            'rating_distribution': get_rating_distribution(soup)}
+    return {
+        "book_id_title": book_id,
+        "book_id": get_id(book_id),
+        "book_title": " ".join(soup.find("h1", {"id": "bookTitle"}).text.split()),
+        "book_description": get_description(soup),
+        "book_url": url,
+        "book_image": soup.find("img", {"id": "coverImage"}).attrs.get("src"),
+        "book_series": get_series_name(soup),
+        "book_series_uri": get_series_uri(soup),
+        "year_first_published": get_year_first_published(soup),
+        "author": " ".join(soup.find("span", {"itemprop": "name"}).text.split()),
+        "author_url": soup.find("a", {"class": "authorName"}).attrs.get("href"),
+        "num_pages": get_num_pages(soup),
+        "genres": get_genres(soup),
+        "num_ratings": int(
+            soup.find("meta", {"itemprop": "ratingCount"})["content"].strip()
+        ),
+        "num_reviews": int(
+            soup.find("meta", {"itemprop": "reviewCount"})["content"].strip()
+        ),
+        "average_rating": float(
+            soup.find("span", {"itemprop": "ratingValue"}).text.strip()
+        ),
+        "rating_distribution": get_rating_distribution(soup),
+    }
