95356da46d79a2bdba19aca4b433ac76a8aedb18
Yash Totale <totaleyash@gmail.com>
2021-07-28 14:00:59 -0700

    v0.0.10

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="goodreads-user-scraper",
-    version="0.0.9",
+    version="0.0.10",
     description="Scrape user data from Goodreads",
     long_description=README,
     long_description_content_type="text/markdown",
