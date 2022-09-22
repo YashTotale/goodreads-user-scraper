598f876b2994eb7e5fc6f4acfd37b3a405e168b5
karlwebstersponge <19327709+ktasper@users.noreply.github.com>
2022-09-22 21:52:47 +0100

    Added poetry

--- src/goodreads_scraper/main.py
+++ src/goodreads_scraper/main.py
@@ -1,8 +1,9 @@
 import argparse
 import os
 
-from scraper import shelves
-from scraper import user
+from .scraper import shelves
+from .scraper import user
+
 
 
 def scrape_user(args: argparse.Namespace):
